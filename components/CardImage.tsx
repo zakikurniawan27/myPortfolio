7ba2da5541c2d9205ce0c121caf2ec50dfe1e4e0
Muhammad Zaki kurniawan <kurniawan.zaki279@gmail.com>
2023-12-27 18:01:50 +0700

    fix: project

--- components/CardImage.tsx
+++ components/CardImage.tsx
@@ -6,7 +6,7 @@ import projectku3 from '../assets/image/projectku3.jpg'
 const CardImage = () => {
   return (
     <>
-        <div className='grid grid-cols-3 sm:grid-cols-1 md:grid-cols-3 gap-4'>
+        <div className='grid grid-cols-3 sm:grid-cols-1 md:grid-cols-3 gap-4 sm:px-5'>
             <div className='bg-white rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 group relative block overflow-hidden rounded-md transition-all duration-500'>
                 <a className=' transition-all duration-500 group-hover:scale-105 tobii-zoom' href='https://challenge-04-todoapp-j10nukeoa-zakikurniawan27.vercel.app/'>
                     <img src='/assets/image/projectku1.jpg' alt='fotoProject'/>
@@ -18,7 +18,7 @@ const CardImage = () => {
                 </a>
             </div>
             <div className='bg-white rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 group relative block overflow-hidden rounded-md transition-all duration-500'>
-                <a className=' transition-all duration-500 group-hover:scale-105 tobii-zoom' href='https://movielist-app-challenge-07.vercel.app/'>
+                <a className=' transition-all duration-500 group-hover:scale-105 tobii-zoom' href='https://movie-list-react-app.vercel.app/'>
                     <img src='/assets/image/projectku2.jpg' alt='fotoProject'/>
                     <div className='absolute -bottom-52 group-hover:bottom-2 left-2 right-2 transition-all duration-300 group-hover:bg-red-600 p-4 rounded shadow '>
                         <p className='text-lg text-white transition duration-300 font-medium'>
